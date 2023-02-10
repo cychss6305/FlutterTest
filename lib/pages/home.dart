be7d2b6faeedda2c4cbe4c078550b6726d0cdbea
at3252張敬賢 <jim30425@gmail.com>
2023-02-10 16:00:40 +0800

    收合式選單

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('Manage Products'),
+                onTap: () {},
+              )
+            ],
+          ),
+        ),
         appBar: AppBar(title: Text('商品列表')),
         body: ProductManager(
             startingProduct: {"title": 'test', 'image': 'asset/123.png'}));
